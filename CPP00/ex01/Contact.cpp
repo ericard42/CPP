cff6285f7a5149bc1ed6c5dc3f2b0c60812a8e3c
Emily FLYNN Elodie RICARD <ericard@student.42.fr>
2021-11-17 15:07:59 +0100

    Phone Book

---
+++ CPP00/ex01/Contact.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Contact.hpp"
+
+Contact::Contact() {
+
+}
+
+Contact::~Contact() {
+
+}
+
---
+++ CPP00/ex01/Phonebook.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Contact.hpp"
+
+int		main()
+{
+	std::cout << "Welcome on your Phone Book !" << std::endl;
+	std::cout << "Choose a command : [ADD] - [SEARCH] - [EXIT]"
+	
+}
