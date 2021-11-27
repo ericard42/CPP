6ca723696a3e5a4aee94e26f08088fca34a1b65d
Emily FLYNN Elodie RICARD <ericard@student.42.fr>
2021-11-27 12:48:59 +0100

    ex06

--- CPP01/ex06/Karen.cpp
+++ CPP01/ex06/Karen.cpp
@@ -37,6 +37,7 @@ void	Karen::complain(std::string level)
 			case 3: this->error(); break;
 			default: this->insignificant(); break;
 		}
+		std::cout << std::endl;
 		if (this->_level < 3)
 			this->_level++;
 		else
@@ -49,7 +50,6 @@ void	Karen::debug (void)
 	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love to get extra bacon or my 7XL-double-cheese-triple-pickle-special-ketchup burger. " << std::endl;
 	std::cout << "I just love it!" << std::endl;
-	std::cout << std::endl;
 }
 
 void	Karen::info (void)
@@ -57,7 +57,6 @@ void	Karen::info (void)
 	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon cost more money. " << std::endl;
 	std::cout << "You don’t put enough! If you did I would not have to ask for it!" << std::endl;
-	std::cout << std::endl;
 }
 
 void	Karen::warning (void)
@@ -65,18 +64,15 @@ void	Karen::warning (void)
 	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. " << std::endl;
 	std::cout << "I’ve been coming here for years and you just started working here last month." << std::endl;
-	std::cout << std::endl;
 }
 
 void	Karen::error (void)
 {
 	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
-	std::cout << std::endl;
 }
 
 void	Karen::insignificant (void)
 {
 	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-	std::cout << std::endl;
 }
