55a8ad6266e0dfee72f6e410c471f5e7b177c658
Emily FLYNN Elodie RICARD <ericard@student.42.fr>
2021-12-13 15:50:03 +0100

    serialize

---
+++ CPP06/ex01/main.cpp
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <iostream>
+#include <string>
+
+struct Data
+{
+	std::string name;
+};
+
+uintptr_t serialize(Data *ptr)
+{
+
+}
+
+Data *deserialize(uintptr_t raw)
+{
+
+}
+
+int main()
+{
+
+}
