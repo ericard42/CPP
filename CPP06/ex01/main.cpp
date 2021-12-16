c9cefa9d080163ee9aa78c863edd8ebf544ab961
Emily FLYNN Elodie RICARD <ericard@student.42.fr>
2021-12-16 10:42:51 +0100

    prout

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -5,19 +5,32 @@
 struct Data
 {
 	std::string name;
+	int 		age;
 };
 
 uintptr_t serialize(Data *ptr)
 {
-
+	return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data *deserialize(uintptr_t raw)
 {
-
+	return reinterpret_cast<Data *>(raw);
 }
 
 int main()
 {
+	Data	*ptr = new Data;
+	Data	*new_ptr;
+	uintptr_t raw;
 
+	ptr->name = "Roger";
+	ptr->age = 42;
+	raw = serialize(ptr);
+	new_ptr = deserialize(raw);
+	std::cout << raw << std::endl;
+	std::cout << new_ptr->name << " is " << new_ptr->age << " years old" << std::endl;
+	std::cout << ptr << " and " << new_ptr << " and " << &raw << std::endl;
+
+	delete ptr;
 }
